95f1b4e381b8c999e051a05279951a8c7ee97713
JoaoCM102 <joao.lima.external@atos.net>
2024-04-22 15:54:42 +0100

    User Entity
    User Entity realizado

---
+++ src/main/java/com/grupo2/aulavirtual/Entities/UserEntity.java
@@ -0,0 +1,76 @@
+package com.grupo2.aulavirtual.Entities;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="user", uniqueConstraints = {@UniqueConstraint(columnNames = {"email"})})
+public class UserEntity implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long idUsuario;
+    @Basic
+    @Column(nullable = false)
+    String email;
+    @Column(nullable = false)
+    String lastname;
+    String firstname;
+    String username;
+    String password;
+    
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+      return List.of(new SimpleGrantedAuthority((role.name())));
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+       return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+       return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @OneToOne(
+        cascade = CascadeType.ALL
+    )
+    @JoinColumn(
+    		name = "adress",
+    		referencedColumnName = "idAdress")
+     AdressEntity adress;
+     
+
+    @OneToOne(
+        cascade = CascadeType.ALL
+    )
+    @JoinColumn(
+    		name = "role",
+    		referencedColumnName = "idRole")
+     Role role ;
+
+}
