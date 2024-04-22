0aff5e4f6e1e544724620fa45f84f7675f4f9df8
Luis David Alvarez Perdigon <luis.alvarez.external@atos.net>
2024-04-22 15:12:48 +0100

    Start project

---
+++ src/test/java/com/grupo2/aulavirtual/AulaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.aulavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
