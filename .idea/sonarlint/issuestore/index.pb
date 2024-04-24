d44bb78e7e9b4cbbe743895e31fc77b347186d1d
David Álvarez <132286769+Davidenk0v@users.noreply.github.com>
2024-04-24 16:27:06 +0100

    ready for merge

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,8 +3,6 @@
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
-w
-Gsrc/main/java/com/grupo2/aulavirtual/Config/Mappers/SecurityConfig.java,7\7\77fe5d4be44494f04c5692651a22a450d3dab7e7
 o
 ?src/main/java/com/grupo2/aulavirtual/Config/SecurityConfig.java,8\6\868d5724a7c4eea7919337678401c42262f6c77a
 o
--- src/main/java/com/grupo2/aulavirtual/Config/SecurityConfig.java
+++ src/main/java/com/grupo2/aulavirtual/Config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,6 +15,11 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(httpSecurity)
+                .authorizeHttpRequests(http -> http.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth -> {
+                    oauth.jwt(jwt->{});
+                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
