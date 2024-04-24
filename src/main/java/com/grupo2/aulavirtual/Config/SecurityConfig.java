8b81aa2ba7840933b9cd3630df026a9a831ec173
David Álvarez <132286769+Davidenk0v@users.noreply.github.com>
2024-04-24 14:44:20 +0100

    start security config

--- src/main/java/com/grupo2/aulavirtual/Config/SecurityConfig.java
+++ src/main/java/com/grupo2/aulavirtual/Config/SecurityConfig.java
@@ -1,4 +1,19 @@
-package com.grupo2.aulavirtual.Config.Mappers;
+package com.grupo2.aulavirtual.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(httpSecurity)
+    }
 }
