9a2268eb53b6f402cce9bb596d5c97923b9ddfec
David Álvarez <132286769+Davidenk0v@users.noreply.github.com>
2024-04-25 17:04:34 +0100

    refactor controllers and get ready for merge

--- src/main/java/com/grupo2/aulavirtual/Controllers/CategoryController.java
+++ src/main/java/com/grupo2/aulavirtual/Controllers/CategoryController.java
@@ -8,7 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/category")
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1/categories")
 public class CategoryController {
 
     // @Autowired
