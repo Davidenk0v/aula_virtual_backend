614620456abb0eba4a7ac1c27f014bbb03f0d94f
David Álvarez <132286769+Davidenk0v@users.noreply.github.com>
2024-04-29 12:56:21 +0100

    fixed merge errors

--- src/main/java/com/grupo2/aulavirtual/services/impl/KeycloackServiceImpl.java
+++ src/main/java/com/grupo2/aulavirtual/services/impl/KeycloackServiceImpl.java
@@ -3,9 +3,12 @@ package com.grupo2.aulavirtual.services.impl;
 import com.grupo2.aulavirtual.payload.keycloack.UserDtoKeycloack;
 import com.grupo2.aulavirtual.services.KeycloackService;
 import com.grupo2.aulavirtual.util.KeycloackProvider;
+import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
+import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
@@ -32,14 +35,31 @@ public class KeycloackServiceImpl implements KeycloackService {
     @Override
     public String createUser(@NonNull UserDtoKeycloack userDTO) {
         int status = 0;
-        UsersResource userResource = KeycloackProvider.getUserResource();
+        UsersResource userResource = KeycloackProvider.getUserResource(null);
 
         UserRepresentation userRepresentation = new UserRepresentation();
         userRepresentation.setFirstName(userDTO.firstname());
+        userRepresentation.setLastName(userDTO.lastname());
+        userRepresentation.setEmail(userDTO.email());
+        userRepresentation.setUsername(userDTO.username());
+        userRepresentation.setEmailVerified(true);
+        userRepresentation.setEnabled(true);
 
+        Response response = userResource.create(userRepresentation);
 
-        userRepresentation.setFirstName(userDTO.firstname());
+        status = response.getStatus();
 
+        if(status == 201){
+            String path = response.getLocation().getPath();
+            String userId = path.substring(path.lastIndexOf("/") + 1);
+
+            CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+            credentialRepresentation.setTemporary(false);
+            credentialRepresentation.setType(OAuth2Constants.PASSWORD);
+            credentialRepresentation.setValue(userDTO.password());
+
+            userResource.get(userId).resetPassword(credentialRepresentation);
+        }
         return null;
     }
 
