75a2330f5b509906c8b13d093b73361122ac4f5c
David Álvarez <132286769+Davidenk0v@users.noreply.github.com>
2024-05-15 20:30:32 +0100

    password verified system done

--- src/main/java/com/grupo2/aulavirtual/repositories/CategoryRepository.java
+++ src/main/java/com/grupo2/aulavirtual/repositories/CategoryRepository.java
@@ -1,17 +1,14 @@
 package com.grupo2.aulavirtual.repositories;
 
-
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.grupo2.aulavirtual.entities.CategoryEntity;
 
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<CategoryEntity, String>{
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 
     Optional<CategoryEntity> findByCategoryContaining(String categoryName);
 }
