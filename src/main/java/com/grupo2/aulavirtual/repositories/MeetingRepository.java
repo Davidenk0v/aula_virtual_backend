9ca06849c22f16ecdb00f8760014f530822242db
David Álvarez <132286769+Davidenk0v@users.noreply.github.com>
2024-05-22 08:22:31 +0100

    fixed some error

--- src/main/java/com/grupo2/aulavirtual/repositories/MeetingRepository.java
+++ src/main/java/com/grupo2/aulavirtual/repositories/MeetingRepository.java
@@ -1,7 +1,6 @@
 package com.grupo2.aulavirtual.repositories;
 
 import com.grupo2.aulavirtual.entities.MeetingEntity;
-import com.grupo2.aulavirtual.entities.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +9,5 @@ import java.util.List;
 @Repository
 public interface MeetingRepository extends JpaRepository<MeetingEntity, Long> {
 
-
     void deleteByNumberMeeting(Long numberMeeting);
 }
-
-
